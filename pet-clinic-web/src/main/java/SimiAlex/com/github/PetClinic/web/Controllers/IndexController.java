ef286fd2529e7e92dc4432dffbf4417565d8b666
siminica <simineaalexandru@gmail.com>
2020-09-15 14:20:20 +0300

    pet clinic index page and controller added, closes #8

---
+++ pet-clinic-web/src/main/java/SimiAlex/com/github/PetClinic/web/Controllers/IndexController.java
@@ -0,0 +1,17 @@
+package SimiAlex.com.github.PetClinic.web.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndex()
+    {
+        return "index";
+    }
+
+
+
+}
