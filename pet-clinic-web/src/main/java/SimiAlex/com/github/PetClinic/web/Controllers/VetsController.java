bc1345f343bf683c8c99886a47e752eb59ee3b19
siminica <simineaalexandru@gmail.com>
2020-09-18 11:02:04 +0300

    list vets

--- pet-clinic-web/src/main/java/SimiAlex/com/github/PetClinic/web/Controllers/VetsController.java
+++ pet-clinic-web/src/main/java/SimiAlex/com/github/PetClinic/web/Controllers/VetsController.java
@@ -1,14 +1,27 @@
 package SimiAlex.com.github.PetClinic.web.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import SimiAlex.com.github.PetClinic.services.VetService;
 
 @Controller
 public class VetsController {
+
+    private final VetService vetService;
+
+    @Autowired
+    public VetsController(VetService vetService){
+        this.vetService = vetService;
+
+    }
     
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String getVets()
+    public String getVets(Model model)
     {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
