f395b5b22b68f4212a4dbdc813319bc09cceb463
siminica <simineaalexandru@gmail.com>
2020-09-21 18:11:34 +0300

    created pet type service map implementation

---
+++ pet-clinic-data/src/main/java/SimiAlex/com/github/PetClinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package SimiAlex.com.github.PetClinic.services;
+
+import SimiAlex.com.github.PetClinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+    
+    
+}
