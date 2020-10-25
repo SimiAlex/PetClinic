85579433823f1974a9da713e7d132d9a11bde949
siminica <simineaalexandru@gmail.com>
2020-10-25 13:03:08 +0200

    added hibernate entities

--- pet-clinic-data/src/main/java/SimiAlex/com/github/PetClinic/model/Vet.java
+++ pet-clinic-data/src/main/java/SimiAlex/com/github/PetClinic/model/Vet.java
@@ -1,11 +1,24 @@
 package SimiAlex.com.github.PetClinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person 
 {
     // fields    
-    private Set<Speciality> specialities;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+                inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    private Set<Speciality> specialities = new HashSet<>();
 
     // methods
     public Set<Speciality> getSpecialities() 
